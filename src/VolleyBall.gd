14cdb1d6ac66622ef7705f577dd8954cef70456c
Joaquin <you@example.com>
2021-09-09 23:49:10 -0300

    agregue velocidad máxima a la pelota

--- src/VolleyBall.gd
+++ src/VolleyBall.gd
@@ -1,5 +1,8 @@
 extends RigidBody2D
 
+
+var max_speed = 1700
+
 onready var spawn = $spawn
 var punto_izq = false
 var punto_der = false
@@ -16,11 +19,16 @@ func _on_pisoder_body_entered(body):
 	if body.is_in_group("pelota"):
 		punto_izq = true
 		
-		
-		
-	
 	
 func _integrate_forces(state):
+	
+
+
+	if abs(get_linear_velocity().x) > max_speed or abs(get_linear_velocity().y) > max_speed:
+		var new_speed = get_linear_velocity().normalized()
+		new_speed *= max_speed
+		set_linear_velocity(new_speed)
+
 	
 	if punto_der:
 		var xform = state.get_transform()
@@ -39,6 +47,7 @@ func _integrate_forces(state):
 		set_linear_velocity(Vector2(0, -800))
 		set_angular_velocity(0) 
 		Global.score1 += 1
+	
 		
 	
 	
