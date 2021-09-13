5c44e00fe270c3b5120b9da0e4fddc9091f9e877
Joaquin <you@example.com>
2021-09-13 19:16:12 -0300

    limite de puntos añadido

--- src/VolleyBall.gd
+++ src/VolleyBall.gd
@@ -3,12 +3,15 @@ extends RigidBody2D
 
 var max_speed = 1700
 
-onready var spawn = $spawn
 var punto_izq = false
 var punto_der = false
+export var max_points10 = 3
 
 onready var saca_der = get_node("../sacaDer").position
 onready var saca_izq = get_node("../sacaIzq").position
+onready var animation = get_node("../Winner")
+onready var winnerIzq = get_node("../CanvasLayer/winnerIzq")
+onready var winnerDer = get_node("../CanvasLayer/winnerDer")
 
 
 func _on_pisoizq_body_entered(body):
@@ -47,6 +50,18 @@ func _integrate_forces(state):
 		set_linear_velocity(Vector2(0, -800))
 		set_angular_velocity(0) 
 		Global.score1 += 1
+		
+	if Global.score1 == max_points10:
+		winnerIzq.visible = true
+		animation.emitting = true
+		call_deferred("queue_free")
+	
+	if Global.score2 == max_points10:
+		winnerDer.visible = true
+		animation.emitting = true
+		call_deferred("queue_free")
+		
+		
 	
 		
 	
